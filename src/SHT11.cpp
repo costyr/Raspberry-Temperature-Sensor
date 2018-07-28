019352478d62f0258b138f66650ccad6a4af51e0
Costi Rada <costi.rada@hotmail.com>
2018-07-28 08:04:54 +0300

    Added main function and read temperature and humidity.

--- src/SHT11.cpp
+++ src/SHT11.cpp
@@ -4,65 +4,72 @@
 
 #include <bcm2835.h>
 #include <stdio.h>
-#include <third-party\rpi-sht1x\RPi_SHT1x.h>
+#include <rpi-sht1x\RPi_SHT1x.h>
 #include <time.h>
 #include "SHT11.h"
 
-SHT11::SHT11(std::function<void(int)> aTempCallBack, std::function<int> aHumidityCallBack) 
-: mTempCallBack(aTempCallBack),
-  mHumidityCallBack(aHumidityCallBack)
+SHT11::SHT11(std::function<void(int)> aTempCallBack, std::function<int> aHumidityCallBack)
+        : mTempCallBack(aTempCallBack),
+            mHumidityCallBack(aHumidityCallBack)
 {
-    // Set up the SHT1x Data and Clock Pins
-	SHT1x_InitPins();
-	
-	// Reset the SHT1x
-	SHT1x_Reset();
 }
 
- void SHT11::Read() 
- {
-    unsigned char noError = 1;  
+void SHT11::Init()
+{
+    // Wait at least 11ms after power-up (chapter 3.1)
+    delay(20);
+
+  // Set up the SHT1x Data and Clock Pins
+    SHT1x_InitPins();
+
+    // Reset the SHT1x
+    SHT1x_Reset();
+}
+
+void SHT11::Read()
+{
+    unsigned char noError = 1;
 
     // Request Temperature measurement
-	noError = SHT1x_Measure_Start( SHT1xMeaT );
-	if (!noError) 
-      return;
-		
+    noError = SHT1x_Measure_Start(SHT1xMeaT);
+    if (!noError)
+        return;
+
     value temp_val;
 
-	// Read Temperature measurement
-	noError = SHT1x_Get_Measure_Value( (unsigned short int*) &temp_val.i );
-	if (!noError)
-	  return;
+    // Read Temperature measurement
+    noError = SHT1x_Get_Measure_Value((unsigned short int *)&temp_val.i);
+    if (!noError)
+        return;
 
     value humi_val;
 
     // Request Humidity Measurement
-	noError = SHT1x_Measure_Start( SHT1xMeaRh );
-	if (!noError)
-	  return;
-		
-	// Read Humidity measurement
-	noError = SHT1x_Get_Measure_Value( (unsigned short int*) &humi_val.i );
-	if (!noError)
-	  return;
+    noError = SHT1x_Measure_Start(SHT1xMeaRh);
+    if (!noError)
+        return;
 
-	// Convert intergers to float and calculate true values
-	temp_val.f = (float)temp_val.i;
-	humi_val.f = (float)humi_val.i;
-	
-	// Calculate Temperature and Humidity
-	SHT1x_Calc(&humi_val.f, &temp_val.f);
+    // Read Humidity measurement
+    noError = SHT1x_Get_Measure_Value((unsigned short int *)&humi_val.i);
+    if (!noError)
+        return;
+
+    // Convert intergers to float and calculate true values
+    temp_val.f = (float)temp_val.i;
+    humi_val.f = (float)humi_val.i;
+
+    // Calculate Temperature and Humidity
+    SHT1x_Calc(&humi_val.f, &temp_val.f);
 
     if (temp_val.f != mTemperature)
     {
-       mTemperature = temp_val.f;
-       mTempCallBack(mTemperature);
+        mTemperature = temp_val.f;
+        mTempCallBack(mTemperature);
     }
 
     if (humi_val.f != mHumidity)
     {
-       mHumidity = humi_val.f;
-       mHumidityCallBack(mHumidity);
+        mHumidity = humi_val.f;
+        mHumidityCallBack(mHumidity);
     }
- }
+}
--- src/SHT11.h
+++ src/SHT11.h
@@ -5,19 +5,21 @@
 #ifndef _SHT11_H_
 #define _SHT11_H_
 
+#include <functional>
+
 class SHT11
 {
-  public:
-    SHT11(std::function<void(float)> aTempCallBack, std::function<float> aHumidityCallBack);
-    
-    void Read();
-  
-  private:
-    float mTemperature { 0 };
-    float mHumidity { 0 };
+public:
+  SHT11(std::function<void(float)> aTempCallBack, std::function<void(float)> aHumidityCallBack);
 
-    std::function<void(int)> mTempCallBack;
-    std::function<int> mHumidityCallBack;
+  void Read();
+
+private:
+  float mTemperature{0};
+  float mHumidity{0};
+
+  std::function<void(float)> mTempCallBack;
+  std::function<void(float)> mHumidityCallBack;
 }
 
 #endif // _SHT11_H_
