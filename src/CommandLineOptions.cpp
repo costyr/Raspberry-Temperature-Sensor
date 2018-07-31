6e1a9e1e066cb3a4e0f84eb37e46b12b545d059d
Costi Rada <costi.rada@caphyon.com>
2018-07-31 17:01:27 +0300

    Formatted code with clang

--- src/CommandLineOptions.cpp
+++ src/CommandLineOptions.cpp
@@ -1,61 +1,57 @@
 /**
- * 
- * 
+ *
+ *
  */
 
 #include "CommandLineOptions.h"
 
 CommandLineOptions::CommandLineOptions()
 {
-    kOptionHandlers = {
-        { "--server_url", [this](int &aPos, int argc, char *argv[]) 
-          { 
-            if (aPos < argc) 
-              return 1; 
-            mServerURL = argv[aPos]; 
-            aPos++; 
-            return 0; 
-          }
-        },
-        { "--server_token", [this](int &aPos, int argc, char *argv[]) 
-          {   
-            if (aPos < argc) 
-              return 1; 
-            mServerToken = argv[aPos]; 
-            aPos++; 
-            return 0; 
-          }
-        }
-    };
+  kOptionHandlers = { { "--server_url",
+                        [this](int & aPos, int argc, char * argv[]) {
+                          if (aPos < argc)
+                            return 1;
+                          mServerURL = argv[aPos];
+                          aPos++;
+                          return 0;
+                        } },
+                      { "--server_token", [this](int & aPos, int argc, char * argv[]) {
+                         if (aPos < argc)
+                           return 1;
+                         mServerToken= argv[aPos];
+                         aPos++;
+                         return 0;
+                       } } };
 }
 
-int CommandLineOptions::ParseCommandLine(int argc, char *argv[])
+int CommandLineOptions::ParseCommandLine(int argc, char * argv[])
 {
-    for (int i = 0; i < argc; i++)
+  for (int i = 0; i < argc; i++)
+  {
+    string commandLineOption(argv[i]);
+
+    auto it =
+      find_if(kOptionHandlers.begin(), kOptionHandlers.end(),
+              [&commandLineOption](auto & aElem) { return (commandLineOption == aElem.first); });
+
+    if (it != kOptionHandlers.end())
     {
-      string commandLineOption(argv[i]);
-
-      auto it = find_if(kOptionHandlers.begin(), kOptionHandlers.end(), 
-        [&commandLineOption](auto & aElem) { return (commandLineOption == aElem.first); });
-
-      if (it != kOptionHandlers.end()) 
-      {
-        if (it->second(i, argc, argv) != 0)
-          return -1;
-      }
+      if (it->second(i, argc, argv) != 0)
+        return -1;
     }
+  }
 
-    return 0;
+  return 0;
 }
 
-const string &CommandLineOptions::GetServerURL() const
+const string & CommandLineOptions::GetServerURL() const
 {
-    return mServerURL;
+  return mServerURL;
 }
 
-const string &CommandLineOptions::GetServerToken() const
+const string & CommandLineOptions::GetServerToken() const
 {
-    return mServerToken;
+  return mServerToken;
 }
 
 //----------------------------------------------------------------
