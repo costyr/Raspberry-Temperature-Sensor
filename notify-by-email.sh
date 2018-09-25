f1dc077c9e23af3d7bc4ff578f0596fabf3214d5
Costi Rada <costi.rada@caphyon.com>
2018-09-25 16:26:19 +0300

    Create email to send in a temp file

--- notify-by-email.sh
+++ notify-by-email.sh
@@ -1,18 +1,22 @@
-rm mail.txt
-echo -e "To: costyr@gmail.com" >> mail.txt
-echo -e "From: costyrpithermostat@gmail.com" >> mail.txt
-echo -e "Content-type: text/html;" >> mail.txt
+tmpfile=$(mktemp /tmp/mail-XXXXXX.txt)
+echo -e "To: costyr@gmail.com" >> $tmpfile
+echo -e "From: costyrpithermostat@gmail.com" >> $tmpfile
+echo -e "Subject: Thermostat service failed!" >> $tmpfile
+echo -e "MIME-Version: 1.0" >> $tmpfile
+echo -e "Content-type: text/html; charset=utf-8" >> $tmpfile
 IFS=
-LastErrors=$(tail /var/log/syslog)
-IFS='\n'
+LastErrors=$(tail /var/log/thermostat.log)
+IFS=$'\n'
 read -rd '' -a array <<< "$LastErrors"
 
 echo -e >> mail.txt
 echo -e >> mail.txt
-echo -e '<html><body><div style="background: #e9ecef; padding: 10pt;"/>' >> mail.txt
+echo -e '<html><body><div style="background: #e9ecef; padding: 10pt;"/>' >> $tmpfile
 for element in "${array[@]}"
 do
-    echo "<p>$element</p>" >> mail.txt
+    echo "<p>$element</p>" >> $tmpfile
 done
-echo -e "</div></body></html>" >> mail.txt
+echo -e "</div></body></html>" >> $tmpfile
 
+ssmpt costyr@gmail <<< $tmpfile
+rm $tmpfile
